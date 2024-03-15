2df03964cb954a7126a2745b66aacbae5e47612e
Taha18a <154629706+Taha18a@users.noreply.github.com>
2024-03-15 15:58:46 +0500

    Add files via upload

---
+++ Table/app.js
@@ -0,0 +1,8 @@
+document.write("JavaScript Connected")
+// Multiplication table from 2 to 10 using alert
+
+let table = "Multiplication Table of 2:\n";
+for (let i = 1; i <= 10; i++) {
+    table += "2 * " + i + " = " + (2 * i) + "\n";
+}
+alert(table);
